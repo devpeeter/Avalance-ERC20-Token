41039c21dc942366c6bd13c818068ce9df7f569c
Peter Fatukasi <gregieprice@gmail.com>
2024-07-27 22:24:22 +0000

    correction done

--- contracts/MetaERC20Token.sol
+++ contracts/MetaERC20Token.sol
@@ -1,74 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.18;
 
-import "./IMetaERC20Token.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MetaERC20Token is IMetaERC20Token {
-    address public immutable owner;
-    string public tokenName;
-    string public tokenSymbol;
-    uint256 public totalSupply;
-    uint8 public tokenDecimal;
-    uint256 public constant MAX_SUPPLY = 1000000;
-
-    mapping(address => uint256) private userBalances;
+contract MetaERC20Token is ERC20, Ownable {
 
     constructor(
         string memory _tokenName,
-        string memory _tokenSymbol,
-        uint8 decimal
-    ) {
-        tokenName = _tokenName;
-        tokenSymbol = _tokenSymbol;
-        tokenDecimal = decimal;
-        owner = msg.sender;
-    }
+        string memory _tokenSymbol
+    ) ERC20(_tokenName, _tokenSymbol) Ownable(msg.sender) {}
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "UNAUTHORIZED");
-        _;
-    }
 
     function mint(address _account, uint256 _amount) public onlyOwner {
-        require(_amount > 0, "CANNOT_MINT_ZERO_VALUE");
-        uint256 tempSupply = totalSupply + _amount;
-        require(tempSupply <= MAX_SUPPLY, "MAXIMUM_TOKEN_SUPPLY_REACHED");
-
-        totalSupply = totalSupply + _amount;
-        userBalances[_account] = userBalances[_account] + _amount;
-
+        _mint(_account, _amount);
     }
 
-    function balanceOf(address _owner) external view returns (uint256) {
-        return userBalances[_owner];
-    }
-
-    function transfer(address _to, uint256 _value) external  returns (bool) {
-        require(_to != address(0), "ZERO_ADDRESS_NOT_ALLOWED");
-        require(_to != msg.sender, "You cannot trasfer token to yourself");
-
-        uint256 _bal = userBalances[msg.sender];
-        require(_bal >= _value, "INSUFFICIENT_BALANCE");
-
-        uint256 _newBal = _bal - _value;
-        
-        userBalances[msg.sender] = _newBal;
-        userBalances[_to] = userBalances[_to] + _value;
-
-        assert(userBalances[msg.sender] == _newBal);
-
-        emit Transfer(msg.sender, _to, _value);
-        return true;
-    }
+   
 
     function burn(uint96 _amount) external {
 
-        require(userBalances[msg.sender] > 0, "YOU_DO_NOT_HAVE_ANY_TOKEN");
-        require(userBalances[msg.sender] >= _amount, "INSUFFICIENT_TOKEN_BALANCE");
-
-        userBalances[msg.sender] = userBalances[msg.sender] - _amount;
-        totalSupply = totalSupply - _amount;
-
-        userBalances[address(0)] = userBalances[address(0)] + _amount;
+        _burn(msg.sender, _amount);
     }
 }
